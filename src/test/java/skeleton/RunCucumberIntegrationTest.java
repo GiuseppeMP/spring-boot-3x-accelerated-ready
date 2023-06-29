add6f60c06aa6160c0a606aec03ae8ae341a7730
Peppe <giuseppemath@gmail.com>
2023-06-28 21:00:11 -0300

    chore(config) remove cucumber from unit suite

--- src/test/java/skeleton/RunCucumberIntegrationTest.java
+++ src/test/java/skeleton/RunCucumberIntegrationTest.java
@@ -7,5 +7,5 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("skeleton")
-public class RunCucumberTest {
+public class RunCucumberIntegrationTest {
 }
